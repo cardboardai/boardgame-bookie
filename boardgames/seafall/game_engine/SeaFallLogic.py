8a76b80f05e8c9a2b011f6e29c18f2c49dee2bc4
karhohs <gandalfisarockstar@gmail.com>
2018-06-05 16:11:32 -0400

    nearly fell into the mutable default value for class attribute trap

--- boardgames/seafall/game_engine/SeaFallLogic.py
+++ boardgames/seafall/game_engine/SeaFallLogic.py
@@ -6,7 +6,8 @@ PATH_MAP_ADJACENCY_INIT = "map_adjacency_init.xlsx"
 class Board():
     def __init__(self, game):
         """
-        This object contains a list of all the coordinates for the SeaFall game board stored in hexgrid_keys.
+        This object contains a list of all the coordinates for the SeaFall game
+        board stored in hexgrid_keys.
         """
         try:
             df = pd.read_excel(PATH_MAP_ADJACENCY_INIT)
@@ -29,21 +30,64 @@ class Board():
 
 class Ship():
     # Rules, pg 8, "Province Boards" also inlcude information about ships
-    def __init__(self, hold=[], upgrades=[], values=(1, 1, 1, 1), vmax=(5, 5, 5, 5)):
+    def __init__(self):
+        self.damage = []
         # hold, a list of objects with max length hold
-        self.hold = hold
+        self.hold = []
         # upgrades, a list of upgrade objects of max length 2
-        self.upgrades = upgrades
+        self.upgrades = []
         # values (explore, hold, raid, sail)
+        self.values = (1, 1, 1, 1)
+        # vmax is the maximum number values can reach for (explore, hold, raid,
+        # sail)
+        self.vmax = (5, 5, 5, 5)
+
+    @property
+    def values(self):
+        return self.values
+    
+    @values.setter
+    def values(self, values):
+        if not isinstance(values, tuple):
+            err_str = ("Not a valid data type. The data type should be a tuple"
+                       " of 4 length.")
+            raise ValueError(err_str)
+        elif len(values) != 4:
+            err_str = ("Not a valid data type. The data type should be a tuple"
+                       " of 4 length.")
+            raise ValueError(err_str)
+        
+        for val, vmax in zip(values, self.vmax):
+            if val > vmax:
+                raise ValueError("A ship value exceeds its max.")
+
         self.values = values
-        # vmax is the maximum number values can reach for (explore, hold, raid, sail)
-        self.vmax = vmax
+
+
+class ProvinceBoard():
+    # Rules, pg 8, "Province Boards"
+    def __init__(self):
+        self.active_advisor = None
+        self.at_war_with
+        self.building_sites = []
+        self.council_room = []
+        self.home_enmity
+        self.fields = (4, 4)
+        ship0 = Ship()
+        ship0.values = (2, 3, 2, 2)
+        ship1 = Ship()
+        ship1.values = (3, 2, 2, 2)
+        self.ships = (ship0, ship1)
+        self.treasure_room = []
+        self.vault = 0
+        self.warehouse = []
+
+    def end_of_game():
+        return True
 
 
 class Province():
-    def __init__(self, player):
-        self.field_0 = 4
-        self.field_1 = 4
+
 
 class Colony():
 
@@ -71,28 +115,35 @@ class Structure():
 
 class Leader():
     # Rules, pg 6, "Leader Cards"
-    def __init__(self, appellation=None, fortune=0, improvement=None, name=None, reputation=0, title=None):
+    def __init__(self, appellation=None, fortune=0, improvement=None,
+                 name=None, reputation=0, title=None):
         # Rules, pg 6, "Leader Cards"
-        # appellation is an ability a leader earns at the start of a campaign or as a game end upgrade.
+        # appellation is an ability a leader earns at the start of a campaign
+        # or as a game end upgrade.
         self.appellation = appellation
         # Rules, pg 3, "Fortune Tokens"
         # the number of fortune tokens a player has at the begining of a game
         self.fortune = fortune
-        # improvement is one of a number of stickers that boosts the effect of a particular guild.
+        # improvement is one of a number of stickers that boosts the effect of
+        # a particular guild.
         self.improvement = improvement
         # The name of the leader as given by the player
         self.name = name
         # Rules, pg 3, "Reputation Tokens"
-        # the number of reputation tokens a player has at the beginnig of a game.
+        # the number of reputation tokens a player has at the beginnig of a
+        # game.
         self.reputation = reputation
         # Rules, pg 6, "Titles"
-        # title, the province rank in the overall campaign based on total glory.
+        # title, the province rank in the overall campaign based on total
+        # glory.
         self.title = title
 
 
 class Site():
     def __init__(self):
-        print("hello")
 
 class UpgradeSheet():
+    def __init__(self):
+
+class Enmity():
     def __init__(self):
